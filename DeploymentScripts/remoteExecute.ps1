293299c7f0c10b3887470050a30b8670d33d5e76
Lane Swenka <admin@immglobal.com>
2017-11-21 11:30:46 -0600

    Initial scripts and documentation
    Initial scripts and documentation

---
+++ DeploymentScripts/remoteExecute.ps1
@@ -0,0 +1,99 @@
+﻿<#
+.SYNOPSIS
+    Script to execute a deployment on a Dynamics365 Finance & Operations Enterprise Edition VM
+    See https://github.com/laneswenka/d365-foee-alm
+#>
+param(
+[string] $Directory = $null,
+[string] $ScriptsDirectory = $null,
+[Parameter(Mandatory=$true)]
+[string] $BuildNumber = $null
+)
+
+trap
+{
+    write-host "Errors found"
+    write-host $_
+    exit 1
+}
+
+<#
+.SYNOPSIS
+    Runs AxUpdateInstaller.exe
+#>
+function Execute-Installer([string]$executeArgs, [string]$packageDirectory)
+{
+
+    $deploymentPath = [string]::Format("{0}", $Directory)
+    $installerFile = [string]::Format("{0}\{1}", $packageDirectory, "AxUpdateInstaller.exe")
+
+    $installerInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $installerInfo.FileName = $installerFile
+    $installerInfo.RedirectStandardOutput = $true
+    $installerInfo.UseShellExecute = $false
+    $installerInfo.Arguments = $executeArgs
+
+    $installer = New-Object System.Diagnostics.Process
+    $installer.StartInfo = $installerInfo
+    $installer.Start() | Out-Null
+
+    $stdout = $installer.StandardOutput.ReadToEnd()
+
+    write-host $stdout
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$deploymentPath = [string]::Format("{0}", $Directory)
+$counter = 0
+
+cd $deploymentPath
+
+$zipFiles = Get-ChildItem -Path $deploymentPath -Filter "*.zip"
+
+foreach($zipFile in $zipFiles)
+{
+    Write-Host "Unblocking File $($zipFile.name)..."
+    Unblock-File -Path $zipFile.FullName
+    Write-Host "Successful"
+
+    Write-Host "Extracting..."
+	
+	$packageDirectory = [string]::Format("{0}\{1}_{2}", $deploymentPath, "package", $counter)
+	[System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile.FullName, $packageDirectory)
+	Write-Host "Extracting $($zipFile.name) Finished"
+
+	#Generate Runbook
+	$runbookFile = [string]::Format("{0}_{1}.xml", $BuildNumber, $counter)
+	$runbookId = [string]::Format("{0}_{1}", $BuildNumber, $counter)
+	$defaultTopology = [string]::Format("{0}\{1}", $ScriptsDirectory, "DefaultTopologyData.xml")
+	$defaultServiceModel = [string]::Format("{0}\{1}", $ScriptsDirectory, "DefaultServiceModelData.xml")
+
+	write-host 'Generating Runbook...'
+
+	$args = [string]::Format("generate -runbookId={0} -runbookFile={1} -topologyFile={2} -serviceModelFile={3}", $runbookId, $runbookFile, $defaultTopology, $defaultServiceModel)
+	Execute-Installer -executeArgs $args -packageDirectory $packageDirectory
+
+	write-host "Runbook $($runbookFile) Generated"
+
+	#Import Runbook
+	write-host "Importing Runbook..."
+
+	$args2 = [string]::Format("import -runbookFile={0}", $runbookFile)
+	Execute-Installer -executeArgs $args2 -packageDirectory $packageDirectory
+
+	write-host "Runbook Imported"
+
+	#Execute
+	write-host "Executing..."
+
+	$args3 = [string]::Format("execute -runbookId={0}", $runbookId)
+	Execute-Installer -executeArgs $args3 -packageDirectory $packageDirectory
+
+	write-host "Deployment Complete"
+
+	$counter = $counter + 1
+}
+
+exit 0
+
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # d365-foee-alm
 Microsoft Dynamics365 Finance &amp; Operations ALM Tools
+
+## Overview
+
+This repo contains Powershell scripts and documentation to setup automated releases of Microsoft Dynamics365 Finance & Operations Enterprise Edition ERP system using Release capabilities in VSTS.
+
+  
+
+This provides open examples on how we can leverage VSTS Release Management capabilities in our Implementations or DevOps practices.  Continuous Delivery is a critical component to providing a stream of value to your end users be they your customers, business partners, or back office employees.
+
+## How to setup automated deployments
+###Step One
+Create a new Agent Pool, Queue, and download the Agent to your target system.  In this example, we are downloading the VSTS Agent on our Dynamics TEST environment.
+![Step_One](/step_one.jpg)
+
+###Step Two
+Go to your VSTS project where your automated builds are running (note - the builds are setup automatically when you deploy a Build environment topology in LCS).  
+
+Visit Build and Release -> Releases -> Create release definition
+![Step_One](/step_one.jpg)
+
+###Step Three
+Attach the release definition to your build artifact.  Depending on your branching design, this could be Trunk or some other branch you have created.  Next, optionally create a Release from every new artifact.
+
+Finally, add an environment that represennts your TEST environment. You may optionally set the deployment to be automatic when a Release is created or at some scheduled time.
+![Step_One](/step_one.jpg)
+
+###Step Four
+Visit the Tasks section and update the Agent phase.  Ensure that you set the Agent Queue to be the queue you created in the first step.  Also, ensure that the Agent.Name property is that of your TEST enviornment as you don't want this to accidentally run on your Build server.
+![Step_One](/step_one.jpg)
+
+###Step Five
+Add a Delete Files task.  Feel free to name it as you wish, and select the Source folder you wish to purge.  In this example, I'm using D:\Deployment directory as the D:\ drive is often SSD storage.
+![Step_One](/step_one.jpg)
+
+###Step Six
+Add a Download Build Artifacts task.  I've set my example to download the Packages artifact from our Trunk build.  
+![Step_One](/step_one.jpg)
+
+###Step Seven
+Add a Download Build Artifacts task.  I've set my exammple to download the DeploymentScripts task.  (Note - I've added a custom step to upload these scripts from source control to the DeploymentTasks artifact in the Trunk Build definition)
+![Step_One](/step_one.jpg)
+
+###Step Eight
+Add a Powershell task.  See the Arguments where we are specifying the Deployment directory, the Scripts directory, and the BuildNumber from our Trunk Build artifact.  
+![Step_One](/step_one.jpg)
+
+
+
+
